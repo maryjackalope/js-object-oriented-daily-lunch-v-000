34452af9a0d4e7e52086f40c0401595831413b98
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 22:24:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 class Neighborhood {
     constructor(name) {
-      this.id = ++NeighborhoodId;
+      this.id = ++neighborhoodId;
         this.name = name;
         store.neighborhoods.push(this);
     }
