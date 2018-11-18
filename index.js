5b9bdc385f1cf0234c5d3e33b74bce30e75fe6a8
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 22:27:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,57 @@ class Neighborhood {
 }
 
 class Customer {
+  
+  deliveries() {
+       return store.deliveries.filter(
+           function(delivery) {
+               return delivery.customerId === this.id;
+           }.bind(this)
+       );
+     }
+      customers() {
+       return this.deliveries().map(trip => {
+                 return trip.customer();
+         })
+       }
+ }
+ let customerId = 0;
+ class Customer {
+    constructor(name) {
+        this.id = ++customerId;
+        this.name = name;
+        store.customers.push(this);
+      }
+       trips() {
+       return store.trips.filter(
+           function(trip) {
+               return trip.passengerId === this.id;
+           }.bind(this)
+       );
+   }
+   drivers() {
+    return this.trips().map(trip => {
+              return trip.driver();
+      })
+    }
+}
+ let tripId = 0;
+  class Trip {
+    constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
+  }
+   driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+   passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+ }
+}
